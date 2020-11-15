189d27155a8235203836f0cd67f95bfa99172e90
aum <sweetcandy.aum@gmail.com>
2020-11-15 21:02:44 +0700

    connect database

---
+++ index.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+
+con.connect(function (err) {
+    if (err) { throw err; } else {
+        console.log("Connect!!!");
+
+    }
+
+});
