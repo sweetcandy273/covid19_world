1ad1fe976b0c8aebaaad24704ed6e6fc5591f726
aum <sweetcandy.aum@gmail.com>
2020-11-16 15:07:57 +0700

    add database in con

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: ""
+    password: "",
+    database:"covid19-data"
 });
 
 con.connect(function (err) {
