cacb502be3007c4ef555604535404eb1f26419db
aum <sweetcandy.aum@gmail.com>
2020-11-16 16:12:21 +0700

    connected database

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 var mysql = require('mysql');
-
+//สร้าง Connection
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database:"covid19-data"
+    database: "covid19-data"
 });
 
+//connect Database
 con.connect(function (err) {
     if (err) { throw err; } else {
         console.log("Connect!!!");
+        //อยากให้แสดงอะไรแก้ที่ sql
+        const sql = "select `COL 2`,`COL 66` from `covid19_confirmed`";
+        var total_confirmed;
+        con.query(sql, function (err, result) {
+            if (err) {
+                throw err; 
+            } else {
+
+
+                console.log(result);
+         
+               
+            }
+        });
+       
+
 
     }
 
